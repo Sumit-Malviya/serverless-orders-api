69910cc1715cb8e9b3ab630e4d9ab338db032916
Sumit-Malviya <codewithsumit0611@gmail.com>
2025-03-12 13:53:26 +0530

    Complete refactored code

--- orders_api/read.py
+++ orders_api/read.py
@@ -3,10 +3,11 @@ import boto3
 import os
 from boto3.dynamodb.conditions import Key
 
+dynamodb = boto3.resource('dynamodb')
+table_name = os.environ.get('ORDERS_TABLE')
+
 
 def lambda_handler(event, context):
-    dynamodb = boto3.resource('dynamodb')
-    table_name = os.environ.get('ORDERS_TABLE')
     table = dynamodb.Table(table_name)
     order_id = int(event['pathParameters']['id'])
     response = table.query(KeyConditionExpression=Key('id').eq(order_id))
@@ -14,5 +15,5 @@ def lambda_handler(event, context):
     return {
         'statusCode': 200,
         'headers': {},
-        'body': json.dumps(response['items'])
+        'body': json.dumps(response['Items'])
     }
